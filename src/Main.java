85cb7808e6818d37c48c4a88bb4530cd9fa8043c
nickname00012-git <info@malex.su>
2024-11-23 09:37:53 +0300

    Initial commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,63 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // Heading
+        System.out.println();
+        System.out.println();
+        System.out.println("<<<<  Homework 10 (Strings) >>>>");
+        System.out.println();
+
+
+        int taskN = 0;
+
+        // Task 1
+
+        taskN += 1;
+        System.out.println("Task " + taskN + ".");
+        System.out.println("=======");
+        System.out.println();
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+
+        System.out.println();
+        System.out.println();
+
+        // Task 2
+
+        taskN += 1;
+        System.out.println("Task " + taskN + ".");
+        System.out.println("=======");
+        System.out.println();
+
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println();
+        System.out.println();
+
+        // Task 3
+
+        taskN += 1;
+        System.out.println("Task " + taskN + ".");
+        System.out.println("=======");
+        System.out.println();
+
+        fullName = "Иванов Семён Семёнович";
+//        System.out.println("fullName = " + fullName);
+        fullName = fullName.replace('ё', 'е');
+
+        System.out.println("fullName = " + fullName);
+
+        System.out.println();
+        System.out.println();
+
     }
 }
+
